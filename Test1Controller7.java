bdb724e4cb64c3565c494f4c71a2dd445b9bdd7b
chacha86 <taejincha0619@gmail.com>
2018-02-01 15:35:15 +0900

    test7

---
+++ Test1Controller7.java
@@ -0,0 +1,56 @@
+package uni.wonkwang.exam.test1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test1Controller7 {
+
+	@RequestMapping("goTest7")
+	String goTest6() {
+	
+		return "test7";
+		
+	}
+	
+	@RequestMapping("test7_1")
+	@ResponseBody
+	String test6_1(String testParam) {
+		
+		return "당신이 작성한 문자는 " + testParam + "입니다.";
+		
+	}
+	
+	@RequestMapping("test7_2")
+	@ResponseBody
+	String test7_2(String testParam) {
+		
+		String rst = "funSpring이라고 정확히 적어주세요~";
+		if(testParam.equals("funSpring")) {
+			rst = "잘 적어주셨습니다!";
+		}
+		
+		return rst;
+		
+	}
+
+	
+	@RequestMapping("goTest7_2")
+	String goTest7_2() {
+	
+		return "test7";
+		
+	}
+	
+	//loginId가 spring, password가 12345이면 로그인 성공이라는 문자가 나오도록 해주세요.
+	String test7_3() {
+		
+		String str = "아이디와 비밀번호를 정확히 입력해주세요.";
+		
+		/*여기에서 로그인 체크를 하시면 됩니다.*/
+		
+		return str;
+		
+	}
+}
---
+++ test7.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<script>
+	
+</script>
+<body>
+	<form action="test6_1">
+		<input type="text" name="testParam" />
+		<input type="submit" value="보내기" />
+	</form>
+	
+	<form action="test6_2">
+		<input type="text" name="testParam" placeholder="funSpring을 입력해주세요." />
+		<input type="submit" value="보내기" />
+	</form>
+</body>
+</html>
